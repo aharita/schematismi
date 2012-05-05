f02edaa708e3f5983ea55c93dc22002278918874
unknown <aharita@NSL-AHARITA.nearsoft.local>
2012-05-05 13:11:14 -0700

    working now -- refactoring and testing is next...

--- ConfigurationTool/Program.cs
+++ ConfigurationTool/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using CommandLine;
 using ConfigurationTool.CommandLine;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Text.RegularExpressions;
 
 namespace ConfigurationTool
 {
@@ -21,11 +24,40 @@ namespace ConfigurationTool
             {
                 Console.WriteLine(options.GetUsage());
             }
+
+            Console.ReadLine();
         }
 
         private static void Run(Options options)
         {
-            
+            XDocument doc = XDocument.Load(options.InputFile);
+            XDocument inner;
+            string filePath;
+
+            foreach (var app in doc.Descendants("application"))
+            {
+                foreach (var config in app.Descendants("config"))
+                {
+                    // open file
+                    filePath = config.Attribute("fileName").Value;
+                    inner = XDocument.Load(filePath);
+
+                    foreach (var attribute in config.Descendants("attribute"))
+                    { 
+                        inner.XPathSelectElement(
+                            attribute.Attribute("xpath").Value)
+                                .Attribute(attribute.Attribute("attribute").Value).Value = attribute.Attribute("value").Value;
+                    }
+
+                    foreach (var element in config.Descendants("element"))
+                    {
+                        inner.XPathSelectElement(element.Attribute("xpath").Value).Value = element.Attribute("value").Value;
+                    }
+
+                    // save file
+                    inner.Save(filePath);
+                }
+            }
         }
     }
 }
