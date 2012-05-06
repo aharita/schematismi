de989ae48466becc7a51e4fd0aeedbf0039d85ae
aharita <aharita@nearsoft.com>
2012-05-05 19:15:26 -0700

    lousy unit test

--- ConfigurationTool.Tests/ConfigurationToolTests.cs
+++ ConfigurationTool.Tests/ConfigurationToolTests.cs
@@ -6,22 +6,58 @@
     using System.Text;
     using NUnit.Framework;
     using ConfigurationTool.CommandLine;
+    using System.Xml.Linq;
+    using System.Xml.XPath;
 
     [TestFixture]
     public class ConfigurationToolTests
     {
         [Test]
-        public void Test()
+        public void AllChangesTest()
         {
             Options options = new Options
             {
-                InputFile = "SampleConfiguration.xml"
+                InputFile = "TestFiles/Test1.xml"
             };
 
-            //ConfigurationTool.Program.Run()
+            ConfigurationTool.Program.Run(options);
+            XDocument doc = XDocument.Load("TestFiles/db.config");
 
-            //Assert.AreEqual(250.00F, destination.Balance);
-            //Assert.AreEqual(100.00F, source.Balance);
+            Assert.AreEqual(
+                doc.XPathSelectElement(@"/connectionStrings/add[@name='Default']").Attribute("connectionString").Value, 
+                "Server=DEFAULT;Database=DEFAULT;uid=DEFAULT;pwd=DEFAULT;");
+
+            Assert.AreEqual(
+                doc.XPathSelectElement(@"/connectionStrings/add[@name='Other']").Attribute("connectionString").Value,
+                "Server=OTHER;Database=OTHER;uid=OTHER;pwd=OTHER;");
+
+            Assert.AreEqual(
+                doc.XPathSelectElement(@"/connectionStrings/myElement[@name='test']").Value,
+                "NEW VALUE");
+        }
+
+        [Test]
+        public void SomeChangesTest()
+        {
+            Options options = new Options
+            {
+                InputFile = "TestFiles/Test2.xml"
+            };
+
+            ConfigurationTool.Program.Run(options);
+            XDocument doc = XDocument.Load("TestFiles/db2.config");
+
+            Assert.AreEqual(
+                doc.XPathSelectElement(@"/connectionStrings/add[@name='Default']").Attribute("connectionString").Value,
+                "Server=DEFAULT;Database=DEFAULT;uid=DEFAULT;pwd=DEFAULT;");
+
+            Assert.AreEqual(
+                doc.XPathSelectElement(@"/connectionStrings/add[@name='Other']").Attribute("connectionString").Value,
+                "Server=SERVER;Database=DATABASE;uid=USER;pwd=PASSWORD;");
+
+            Assert.AreEqual(
+                doc.XPathSelectElement(@"/connectionStrings/myElement[@name='test']").Value,
+                "NO CHANGE");
         }
     }
 }
