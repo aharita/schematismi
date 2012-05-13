89ceabd5ad092ee2a2aa02db2b05bf2fcea7bac2
aharita <aharita@nearsoft.com>
2012-05-12 21:47:22 -0700

    cleaned some files

--- Schematismi/CommandLine/Options.cs
+++ Schematismi/CommandLine/Options.cs
@@ -5,7 +5,7 @@ namespace Schematismi.CommandLine
 {
     public class Options
     {
-        private static readonly HeadingInfo headingInfo = new HeadingInfo("ConfigurationTool", "0.1");
+        private static readonly HeadingInfo headingInfo = new HeadingInfo("Schematismi", "0.1");
 
         [Option("i", "input", Required = true, HelpText = "XML configuration file to read.")]
         public string InputFile = null;
@@ -16,7 +16,7 @@ namespace Schematismi.CommandLine
             var help = new HelpText(Options.headingInfo);
             help.AdditionalNewLineAfterOption = true;
             help.Copyright = new CopyrightInfo("aharita", 1983, 2012);
-            help.AddPreOptionsLine("Configuration tool post installation");
+            help.AddPreOptionsLine("Post installation configuration tool");
             //help.AddPreOptionsLine("Usage: SampleApp -rMyData.in -wMyData.out --calculate");
             //help.AddPreOptionsLine(string.Format("       SampleApp -rMyData.in -i -j{0} file0.def file1.def", 9.7));
             //help.AddPreOptionsLine("       SampleApp -rMath.xml -wReport.bin -o *;/;+;-");
