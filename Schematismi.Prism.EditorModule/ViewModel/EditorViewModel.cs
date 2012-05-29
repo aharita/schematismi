dd533910fe4399c194dc977f4e4ed306b3b143d4
aharita <aharita@nearsoft.com>
2012-05-28 21:40:19 -0700

    binding globalcommand with local command - created viewmodelbase for notify

--- Schematismi.Prism.EditorModule/ViewModel/EditorViewModel.cs
+++ Schematismi.Prism.EditorModule/ViewModel/EditorViewModel.cs
@@ -1,8 +1,40 @@
-﻿
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Practices.Prism.Commands;
+using Schematismi.Prism.Infrastructure;
+
 namespace Schematismi.Prism.EditorModule.ViewModel
 {
-    public class EditorViewModel : IEditorViewModel
+    public class EditorViewModel : ViewModelBase, IEditorViewModel, INotifyPropertyChanged
     {
-        public string Message { get; set; }
+        public DelegateCommand TestCommand { get; set; }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+
+            set
+            {
+                _message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        public EditorViewModel()
+        {
+            TestCommand = new DelegateCommand(TestMethod, CanTestMethod);
+            GlobalCommands.SuperTestMethod.RegisterCommand(TestCommand);
+        }
+
+        private bool CanTestMethod()
+        {
+            return true;
+        }
+
+        private void TestMethod()
+        {
+            Message = DateTime.Now.ToString();
+        }
     }
 }
---
+++ Schematismi.Prism.Infrastructure/GlobalCommands.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Practices.Prism.Commands;
+
+namespace Schematismi.Prism.Infrastructure
+{
+    public static class GlobalCommands
+    {
+        public static CompositeCommand SuperTestMethod = new CompositeCommand();
+    }
+}
